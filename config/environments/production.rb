a096e84e7eac6035796c5d58fa8469adbe72a069
aijaz khan <aijaz-khan@hotmail.com>
2020-12-01 09:09:24 +0500

    Update mailer domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,6 +110,9 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   #
+  #
+  config.action_mailer.default_url_options = { host: 'appovation.co' }
+
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
